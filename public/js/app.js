9d92dead29b6e8a5419f02cee86f32ab13a2bf85
SaraHassanAboEldahab <sara.aboeldahab205@gmail.com>
2020-04-19 09:49:36 +0200

    initial commit-2

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,24 @@
-console.log("this is from app.js")
+const form = document.querySelector("form")
+const search = document.querySelector("input")
+const msg1 = document.querySelector("#msg-1")
+const msg2 = document.querySelector("#msg-2")
 
-
-const form=document.querySelector("form")
-const search=document.querySelector("input")
-const msg1=document.querySelector("#msg-1")
-const msg2=document.querySelector("#msg-2")
-
-msg1.textContent="loading ....."
-msg2.textContent=" "
+msg1.textContent = "loading ....."
+msg2.textContent = " "
 
 //fetch API is not part of js but is browser based API 
-form.addEventListener("submit",(e)=>{
+form.addEventListener("submit", (e) => {
     e.preventDefault()
-    loc=search.value
-    fetch("http://localhost:3000/weather?address=" + loc).then((res)=>{ //that means fetch data from this url then run this function
-        res.json().then((data)=>{ // this function will run when json data has arrived and passed
-            if(data.error){
-                msg1.textContent=data.error
-            }else{
-               msg1.textContent=data.location
-               msg2.textContent=data.forecast
+    loc = search.value
+    fetch("http://localhost:3000/weather?address=" + loc).then((res) => { //that means fetch data from this url then run this function
+        res.json().then((data) => { // this function will run when json data has arrived and passed
+            if (data.error) {
+                msg1.textContent = data.error
+            } else {
+                msg1.textContent = data.location
+                msg2.textContent = data.forecast
 
             }
         })
-})
+    })
 })
