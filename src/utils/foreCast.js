814eaa689918c085b43947e5604b6c5f7dd0c58d
SaraHassanAboEldahab <sara.aboeldahab205@gmail.com>
2020-04-20 12:42:51 +0200

    adding forecast data

--- src/utils/foreCast.js
+++ src/utils/foreCast.js
@@ -2,16 +2,14 @@ const request = require("request")
 const foreCast = (latitude, longitude, callback) => {
     const url = "https://api.darksky.net/forecast/b7d4f09a0b230d0475b4732ecfffa9d1/" + latitude + "," + longitude;
 
-    request({ url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback("unable to connect to wether services !!", undefined)
-        }
-        else if (body.error) {
+        } else if (body.error) {
             callback("unable to find location !!", undefined)
 
-        }
-        else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degree out.`)
+        } else {
+            callback(undefined, `${body.daily.data[0].summary} , ${body.daily.data[0].icon} and It is currently ${body.currently.temperature} degree out.`)
         }
     })
 }
